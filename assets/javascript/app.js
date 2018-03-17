32e1d582d6a70c41ab7ceb132f927732d7b91920
Steve Valasek <svalasek@gmail.com>
2018-03-16 17:02:31 -0700

    psuedocoded layout, but giving up to get the train timer done instead.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,7 +18,28 @@ var losses = 0
 var draws = 0
 var pOneGuess = ""
 var pTwoGuess = ""
+var playerName = ""
 
+
+  $("#startBtn").on("click", function()
+  {
+    playerName = ("#playerName").val().trim();
+    $(this).hide();
+    $("#startBox").hide();
+    gamePlay();
+    });
+  
+
+
+// limit to only 2 players or play computer, computer code written in previous game
+function gamePlay() {
+
+
+}
+
+//give choice for 1 or 2 players
+
+//
 
 // update page to show other choices
 database.ref().on("choice", function(snapshot){
@@ -28,8 +49,11 @@ database.ref().on("choice", function(snapshot){
 // show other user's choice after first player chooses
 
 //overwrite player choices each round
-//timer?
+//timer? maybe countdown 15 seconds, display match-up and play again option?
+
 //need to see what other player chooses
+
+//user seeing game is always on left, right is always other player but in code need to show one as p1 and other as p2
 
 
 //choose rock, paper or scissors
@@ -57,6 +81,9 @@ if ((pOneGuess === "r") || (pOneGuess === "p") || (pOneGuess === "s")) {
 
 };
 //tally wins, losses and draws
+
+
+//
 
 //include chat
 
